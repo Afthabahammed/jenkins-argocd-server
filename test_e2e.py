1723daff9b3997ab246c488742d06efa5c23702b
Ubuntu <ubuntu@ip-172-31-43-52.ap-south-1.compute.internal>
2026-09-21 17:38:59 +0000

    Fix pytest test discovery

--- test_e2e.py
+++ test_e2e.py
@@ -4,7 +4,7 @@ from main import app, state
 
 client = TestClient(app)
 
-def run_e2e_tests():
+def test_e2e_tests():
     print("==========================================================================")
     print(" Running End-to-End Automated Integration Tests for Seclock ")
     print("==========================================================================")
